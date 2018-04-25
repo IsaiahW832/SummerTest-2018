0c6d169c1c3f03c47675057a3e33f240a95f7a7f
isaia <isaia@LAPTOP-JLPE994G>
2018-04-25 16:39:06 -0500

    Added Vision code to draw a circle around yellow

--- VisionTesting/src/Vision.cpp
+++ VisionTesting/src/Vision.cpp
@@ -8,7 +8,7 @@ void Vision::VisionThread() {
 	//Create Camera object
 	cs::UsbCamera camera = cs::UsbCamera();
 
-	//Set brightness and exposure-
+	//Set brightness and exposure
 	camera.SetBrightness(5);
 	camera.SetExposureManual(50);
 
@@ -42,32 +42,68 @@ void Vision::VisionThread() {
 //		cv::cvtColor(src, hsv, CV_BGR2HSV);
 
 		//find green from hsv mat-put to threshOutput mat
-		cv::inRange(hsv, cv::Scalar(20, 50, 50), cv::Scalar(30, 255, 255), threshOutput);
+		//cv::inRange(hsv, cv::Scalar(20, 100, 100), cv::Scalar(30, 255, 255), threshOutput);
+		cv::inRange(hsv, cv::Scalar(20, 100, 100), cv::Scalar(30, 255, 255), threshOutput);
 
 		//group pixels into contours
 		cv::findContours(threshOutput, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
 
-		int contoursFound = 0;
+		//Create some lists for circles and stuff
+		std::vector<std::vector<cv::Point>> contours_poly (contours.size());
+		std::vector<cv::Point2f> center(contours.size());
+		std::vector<float> radius(contours.size());
 
+		std::vector<cv::Point2f> centerLarge;
+		std::vector<float> radiusLarge;
+
+		//Variables for Circle Y coordinates and index in array
+		int circleY = src.cols;
+		int circleIndex = -1;
+
+		//create circles around contours
 		for(unsigned int i = 0; i < contours.size(); i++) {
 
-			//if the area of the contour is too small, it will not be drawn
+			//if the contour is too small, it will skip to the next iteration of the loop
 			float contourArea = cv::contourArea(contours[i]);
-			if(contourArea < 20){
-				continue;
+			if(contourArea < 30){continue;}
+
+			cv::approxPolyDP(cv::Mat(contours[i]), contours_poly[i], 3, true);
+			minEnclosingCircle((cv::Mat)contours_poly[i], center[i], radius[i]);
+
+			//if the radius of the circle is too small, it will not be drawn
+			if(radius[i] > 10){
+				circleY = center[i].y;
+				circleIndex = i;
 			}
-			else {
-				contoursFound++;
-			}
-			cv::drawContours(src, contours, i, cv::Scalar(255, 0, 255), 3, 8, hierarchy, 0, cv::Point() );
+
+			cv::drawContours(src, contours_poly, i, cv::Scalar(255, 0, 255), 1, 8, hierarchy, 0, cv::Point() );
 		}
 
-		if(contoursFound > 0){
-			Vision::seeYellow = true;
-		}
-		else{
-			Vision::seeYellow = false;
-		}
+        //Find the average of the x coordinates of the circle.
+        cv::Mat mean;
+        int width = src.cols;
+        //int pixelCenter = width / 2;
+
+        //Only process if circle found
+        if(circleIndex > -1){
+        	//TODO: look into what this does
+        	centerLarge.push_back(center[circleIndex]);
+
+        	//draw circle
+        	 cv::circle(src, center[circleIndex], (int)radius[circleIndex], cv::Scalar(255, 0, 255), 2, 8, 0);
+
+        	 //Find the average of the x coordinates?
+        	 cv::reduce(centerLarge, mean, 1, cv::ReduceTypes::REDUCE_AVG);
+
+        	 //This creates and converts the matrix to a point, and draws a dot.
+        	 cv::Point2f meanPoint(mean.at<float>(0,0), mean.at<float>(0,1));
+        	 cv::circle(src, meanPoint, 3, cv::Scalar(0, 0, 255), -1, 8, 0);
+
+        	 Vision::seeYellow = true;
+        }
+        else{
+        	Vision::seeYellow = false;
+        }
 
 		outputStreamStd.PutFrame(src);
 	}
